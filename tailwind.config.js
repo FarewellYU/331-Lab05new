f8d44770d5a4d63ac191bb10bfdc038c0df2b86e
Haoxuan Yu <haoxuan_yu@cmu.ac.th>
2024-08-30 00:35:53 +0800

    10.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,22 @@ export default {
     extend: {
       boxShadow: {
         'sp': '0 3px 12px 0 rgba(0, 0, 0, 0.2)',
-      }
+      
     },
     fontFamily: {
       sans: ['Avenir', 'Halvetica', 'Arial', 'sans-serif'],
-    }
+    },
+    //this is animation class
+    animation: {
+      fade: 'yellowFade 3s ease-in-out;',
+    },
+    keyframes: {
+      yellowFade:{
+        '0%': { backgroundColor: 'yellow' },
+        '100%': { backgroundColor: 'transparent' },
+      },
+    },
+  },
   },
   plugins: [],
 }
