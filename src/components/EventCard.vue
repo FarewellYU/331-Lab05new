9b58ccb73e0be5a633032bd092e1918a51a9c7f9
Haoxuan Yu <haoxuan_yu@cmu.ac.th>
2024-08-30 00:17:16 +0800

    4.2.1

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -23,10 +23,10 @@ defineProps<{
   border: 1px solid #39495c;
   margin-bottom: 18px;
 }
-.event-card:hover {
+/* .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
+} */
 
 .event-link {
   text-decoration: none;
